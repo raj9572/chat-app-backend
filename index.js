5e431eb2103eb94eaefab75bd69d41cbb130b485
rajali <rajali1432@gmail.com>
2025-09-04 02:04:24 +0530

    spelling mistake or url

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ app.use(express.json())
 app.use(cookieParser())
 
 const corsOptions = {
-  origin:[process.env.CLIENT_UR,"http://localhost:5173"],
+  origin:[process.env.CLIENT_URL ,"http://localhost:5173"],
   credentials:true
 }
 
