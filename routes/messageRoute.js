b6b88733ccf26212a3abd1c95f163439883e2214
rajali <rajali1432@gmail.com>
2025-10-06 15:23:55 +0530

    complete the image-emoji feature

--- routes/messageRoute.js
+++ routes/messageRoute.js
@@ -1,10 +1,11 @@
 import express from "express";
 import {  getMessage, sendMessage } from "../controllers/messageController.js";
 import isAuthenticated from "../middleware/isAuthenticated.js";
+import { upload } from "../middleware/multer.middleware.js";
 
 const router = express.Router();
 
-router.route("/send/:id").post(isAuthenticated,sendMessage);
+router.route("/send/:id").post(isAuthenticated,upload.single("image"),sendMessage);
 router.route("/:id").get(isAuthenticated, getMessage);
 
 export default router;
